a3f83e770a1a9c828a3cc865e8117efbc721a5a9
EC2 Default User <ec2-user@ip-172-31-7-90.us-west-2.compute.internal>
2020-06-30 23:43:24 +0000

    Segunda aplicacion

--- hello_app/app/models/micro_post.rb
+++ hello_app/app/models/micro_post.rb
@@ -1,3 +1,5 @@
 class MicroPost < ApplicationRecord
     belongs_to :user
+    validates :content, length: { maximum: 10 },  presence:true
+    validates :user_id, length: { maximum: 10 },  presence:true
 end
